5cdd35e7eb7098505f925a6da5f7f2b201602790
DanielSve <e.danielsvensson@gmail.com>
2022-09-06 18:54:11 +0200

    reconfigured IT test

--- src/test/java/com/example/springcalc/AppIT.java
+++ src/test/java/com/example/springcalc/AppIT.java
@@ -2,16 +2,26 @@ package com.example.springcalc;
 
 import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class AppIT {
 
-    @BeforeAll
-    public static void setup() {
+    @Before
+    public void setup() {
         RestAssured.baseURI = "http://localhost";
         RestAssured.port = 8080;
     }
