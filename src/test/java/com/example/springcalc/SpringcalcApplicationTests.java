f3951ba42207833128c5c286eb12fea0d1016d5c
DanielSve <e.danielsvensson@gmail.com>
2022-09-06 16:06:00 +0200

    first commit

---
+++ src/test/java/com/example/springcalc/SpringcalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcalcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
