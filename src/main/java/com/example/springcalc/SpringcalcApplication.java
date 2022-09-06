fb816f35be036927a045ed73723b71a189a5cd92
DanielSve <e.danielsvensson@gmail.com>
2022-09-06 20:09:59 +0200

    test

--- src/main/java/com/example/springcalc/SpringcalcApplication.java
+++ src/main/java/com/example/springcalc/SpringcalcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringcalcApplication {
-    String s = "hejsandu";
+    String s = "hejsanduu";
     public static void main(String[] args) {
         SpringApplication.run(SpringcalcApplication.class, args);
     }
