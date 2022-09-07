8ddf25e6dd18a64e1fc2fdb409dab739bdda52a7
DanielSve <e.danielsvensson@gmail.com>
2022-09-07 12:37:42 +0200

    test

--- src/test/java/com/example/springcalc/AppIT.java
+++ src/test/java/com/example/springcalc/AppIT.java
@@ -4,10 +4,13 @@ import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static io.restassured.RestAssured.when;
 
-//@ExtendWith(SpringExtension.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class AppIT {
 
